aad54ac302ebf67634943f3866f58f1cf88d91ea
ポール ウェッブ <netopwibby@thenetwork.email>
2018-04-24 14:55:53 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/* jshint undef: true, unused: true, esversion: 6, node: true */
+
+"use strict";
+
+
+
+/*
+  Keyword(s) (to activate NetNevi)
+    - name
+    - nickname
+
+  Events (could also be used to activate NetNavi)
+    - alarm
+    - charging, add to charger, remove from charger (all these for devices)
+
+  Listening sensitivity
+*/
